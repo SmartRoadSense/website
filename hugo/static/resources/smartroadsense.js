7272bfb5a80b68e3be3c7c4239b6dc57fae5803e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-06-01 18:38:46 +0200

    Update JS clipboard copy to use navigator.clipboard API
    See https://developers.google.com/web/updates/2018/03/clipboardapi

--- hugo/static/resources/smartroadsense.js
+++ hugo/static/resources/smartroadsense.js
@@ -16,20 +16,16 @@ function thousands_commas(input, adapt = true) {
 }
 
 // Taken from https://stackoverflow.com/a/33928558/3118
-function copy_to_clipboard(text) {
-    if (window.clipboardData && window.clipboardData.setData) {
-        // IE specific code path to prevent textarea being shown while dialog is visible.
-        return clipboardData.setData("Text", text);
-    }
-    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+function copy_to_clipboard_legacy(text) {
+    if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
         var textarea = document.createElement("textarea");
         textarea.textContent = text;
-        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
         document.body.appendChild(textarea);
         textarea.select();
 
         try {
-            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+            return document.execCommand("copy"); // Security exception may be thrown by some browsers
         }
         catch (ex) {
             console.warn("Copy to clipboard failed.", ex);
@@ -38,6 +34,28 @@ function copy_to_clipboard(text) {
         finally {
             document.body.removeChild(textarea);
         }
+    }
+    else {
+        return false;
+    }
+}
+async function copy_to_clipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path
+        return clipboardData.setData("Text", text);
+    }
+    else if(navigator.clipboard) {
+        try {
+            await navigator.clipboard.writeText(text);
+            return true;
+        }
+        catch(err) {
+            console.warn('Failed using navigator.clipboard copy.', err);
+            return copy_to_clipboard_legacy(text);
+        }
+    }
+    else {
+        return copy_to_clipboard_legacy(text);
     }
 }
 
@@ -117,11 +135,11 @@ $(document).ready(function() {
     }
 
     // BibTeX loading
-    $('.bibliography .bibtex-link a').click(function(event) {
+    $('.bibliography .bibtex-link a').click(async function(event) {
         var bib = $(this).data('bibtex');
         $('.bibliography .bibtex').fadeIn().find('.output').text(bib);
 
-        if(copy_to_clipboard(bib))
+        if(await copy_to_clipboard(bib))
             $('.bibliography .bibtex .copy-confirmation').show();
         else
             $('.bibliography .bibtex .copy-confirmation').hide();
