2f20fead6194955699f47de2b41e011008d084a7
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-02-23 23:35:48 +0100

    Use IntersectionObserver for header animations

--- hugo/assets/js/smartroadsense.js
+++ hugo/assets/js/smartroadsense.js
@@ -73,8 +73,6 @@ function update_stats() {
     });
 }
 
-var listOfScrollableElements = [];
-
 function srs_onscroll(eventObject, quick) {
     var scrollTop = $(document).scrollTop();
     if(scrollTop > 10) {
@@ -83,42 +81,24 @@ function srs_onscroll(eventObject, quick) {
     else {
         $('nav.navbar').removeClass('scrolled');
     }
-
-    if(listOfScrollableElements.length > 0) {
-        if(listOfScrollableElements[0].yOffset < (scrollTop + ($(window).height() * 0.9))) {
-            if(quick) {
-                $(listOfScrollableElements[0].element).addClass('no-trans');
-            }
-            $(listOfScrollableElements[0].element).removeClass('scroll-in');
-            listOfScrollableElements.splice(0, 1);
-        }
-    }
 }
 
 $(document).ready(function() {
-    // Build scrollable element list
-    $('.scroll-in').each(function(index, element) {
-        var yPos = $(element).offset().top;
-
-        listOfScrollableElements.push({
-            element: element,
-            yOffset: yPos
+    // Scroll observers
+    const itemScrollObserver = new IntersectionObserver(function(entries, obs) {
+        entries.forEach(entry => {
+            console.log('Observed item ' + entry.target + ', intersection ratio ' + entry.intersectionRatio);
+            if(entry.intersectionRatio >= 0.5) {
+                $(entry.target).removeClass('scroll-in');
+                obs.unobserve(entry.target);
+            }
         });
+    }, { threshold: 0.5 });
+    document.querySelectorAll('.scroll-in').forEach(item => {
+        itemScrollObserver.observe(item);
     });
-    listOfScrollableElements.sort(function(a, b) { return a.yOffset - b.yOffset; });
 
-    srs_onscroll(null, true);
-
-    // Scroll handling
     $(window).scroll(srs_onscroll);
-
-    // When/if window resizes, execute all animations since positions are not guaranteed anymore
-    $(window).resize(function() {
-        for(var i = 0; i < listOfScrollableElements.length; ++i) {
-            $(listOfScrollableElements[i].element).addClass('no-trans').removeClass('scroll-in');
-        }
-        listOfScrollableElements = [];
-    });
 
     // Navbar collapse/expand
     $('nav.navbar').on('show.bs.collapse', function () {
